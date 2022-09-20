a80b1ca048f758771fdfacb62bcc9985bbcc8017
Mustansar Saeed <m.mustansar.saeed@gmail.com>
2022-09-20 12:26:46 +0500

    - Conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import MoviesList from "./components/MoviesList";
 import { getMovies, deleteMovie } from "./services/fakeMovieService";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [movies, setMovies] = useState(getMovies());
@@ -12,12 +12,22 @@ function App() {
     setMovies(getMovies());
   }
 
-  return (
-    <main className="container">
-      <p>Showing {getMovies().length} movies in the database</p>
-      <MoviesList movies={getMovies()} onDelete={handleOnDelete} />
-    </main>
-  );
+  console.log("Rendering changes: movies.length= " + movies.length);
+  useEffect(() => {
+    console.log("Effects: movies.length = " + movies.length);
+  }, [movies]);
+
+  console.log("Returning render: movies.length= " + movies.length);
+  if (movies.length === 0) {
+    return <p>Showing {movies.length} movies in the database</p>;
+  } else {
+    return (
+      <main className="container">
+        <p>Showing {movies.length} movies in the database</p>
+        <MoviesList movies={movies} onDelete={handleOnDelete} />
+      </main>
+    );
+  }
 }
 
 export default App;
