a3d0b2d28b41cdefca51e740b106744728a38f42
Mustansar Saeed <m.mustansar.saeed@gmail.com>
2022-09-22 16:51:51 +0500

    - Filtering and list grouping completed

--- src/App.js
+++ src/App.js
@@ -2,35 +2,34 @@ import logo from "./logo.svg";
 import "./App.css";
 import MoviesList from "./components/MoviesList";
 import { getMovies, deleteMovie, getMovie } from "./services/fakeMovieService";
+import { getGenres } from "./services/fakeGenreService";
 import { useEffect, useState } from "react";
 import Pagination from "./components/common/pagination";
 import { paginate } from "./utils/paginator";
+import ListGroup from "./components/common/listgroup";
 function App(props) {
   const [latestMovies, setMovies] = useState(getMovies());
   const [pageSize, setPageSize] = useState(4);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const pagedMovies = paginate(latestMovies, currentPage, pageSize);
+  const [genres, setGenres] = useState(getGenres());
+  const [currentGenre, setCurrentGenre] = useState({});
+
+  let allGenres = [{ name: "All Genres" }, ...genres];
+  const filteredMovies =
+    currentGenre && currentGenre._id
+      ? latestMovies.filter((movie) => movie.genre._id === currentGenre._id)
+      : latestMovies;
+
+  const pagedMovies = paginate(filteredMovies, currentPage, pageSize);
 
   function handleOnDelete(movieId) {
     deleteMovie(movieId);
     setMovies(() => getMovies());
-
-    console.log(
-      "movieId: ",
-      movieId,
-      " movies,length=",
-      latestMovies.length,
-      " getMovies.length: ",
-      getMovies().length
-    );
   }
 
   function handleOnLike(movie) {
     let movies = getMovies();
-    // let fMovie = movies.filter((m) => {
-    //   return m._id == movie._id;
-    // })[0];
     let index = movies.indexOf(movie);
     movies[index].liked = !movies[index].liked;
     setMovies(movies);
@@ -42,25 +41,38 @@ function App(props) {
     setCurrentPage(page);
   }
 
-  console.log("Returning render: movies.length= " + latestMovies.length);
-  if (latestMovies.length === 0) {
-    return <p>Showing {latestMovies.length} movies in the database</p>;
+  function handleOnGenreChange(genre) {
+    console.log(genre);
+    setCurrentGenre(genre);
+  }
+
+  if (pagedMovies.length === 0) {
+    return <p>Showing {pagedMovies.length} movies in the database</p>;
   } else {
     return (
-      <main className="container">
-        <p>Showing {pagedMovies.length} movies in the database</p>
-        <MoviesList
-          movies={pagedMovies}
-          onDelete={handleOnDelete}
-          onLike={handleOnLike}
-          onLikeClicked={handleOnLike}
-        />
-        <Pagination
-          itemsCount={latestMovies.length}
-          pageSize={pageSize}
-          onPageChange={handleOnPageChange}
-          currentPage={currentPage}
-        />
+      <main className="container row m-5">
+        <div className="col-2">
+          <ListGroup
+            items={allGenres}
+            currentItem={currentGenre}
+            onItemSelected={handleOnGenreChange}
+          />
+        </div>
+        <div className="col">
+          <p>Showing {filteredMovies.length} movies in the database</p>
+          <MoviesList
+            movies={pagedMovies}
+            onDelete={handleOnDelete}
+            onLike={handleOnLike}
+            onLikeClicked={handleOnLike}
+          />
+          <Pagination
+            itemsCount={filteredMovies.length}
+            pageSize={pageSize}
+            onPageChange={handleOnPageChange}
+            currentPage={currentPage}
+          />
+        </div>
       </main>
     );
   }
---
+++ src/components/common/listgroup.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+function ListGroup(props) {
+  const { items, onItemSelected, currentItem, textProperty, valueProperty } =
+    props;
+  return (
+    <ul className="list-group">
+      {items.map((item) => {
+        return (
+          <li
+            key={item[valueProperty]}
+            className={
+              currentItem._id === item[valueProperty]
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            style={{ cursor: "pointer" }}
+            onClick={() => onItemSelected(item)}
+          >
+            {item[textProperty]}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+ListGroup.defaultProps = {
+  textProperty: "name",
+  valueProperty: "_id",
+};
+export default ListGroup;
